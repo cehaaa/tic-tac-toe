e69a239c48d0b4e6c7bc9e940497b4ee4b786741
Christian Gouw <55861341+veeloo@users.noreply.github.com>
2022-05-06 23:04:57 +0700

    feat: adding custom font and new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				game: "#202136",
+				purple: "#7420dc",
+				yellow: "#ffae15",
+				board: "#373751",
+			},
+		},
 	},
 	plugins: [],
 };
