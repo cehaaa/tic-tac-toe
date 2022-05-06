6b3a25aad85dfe55b248cce254ef9a977b7d39be
Christian Gouw <55861341+veeloo@users.noreply.github.com>
2022-05-07 00:45:00 +0700

    feat: add XO icon

---
+++ src/components/Icons/OICon.tsx
@@ -0,0 +1,12 @@
+const OICon = () => {
+	return (
+		<div>
+			{/* <div className='h-20 w-20 rounded-full border-8 border-yellow'></div> */}
+			<div className='flex items-center justify-center h-24 w-24 bg-yellow rounded-full'>
+				<div className='h-16 w-16 rounded-full bg-gray-100'></div>
+			</div>
+		</div>
+	);
+};
+
+export default OICon;
---
+++ src/components/Icons/XIcon.tsx
@@ -0,0 +1,10 @@
+const XIcon = () => {
+	return (
+		<div className='relative'>
+			<div className='h-4 w-20 absolute z-10 bg-purple rotate-45'></div>
+			<div className='h-4 w-20 bg-purple -rotate-45'></div>
+		</div>
+	);
+};
+
+export default XIcon;
